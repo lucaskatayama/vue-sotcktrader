df4519b1266af3613cec2a3978771c7845af92fc
Lucas Katayama <lucaskatayama@gmail.com>
2017-05-28 13:10:39 -0300

    Setting up Vuex State Management

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -16,15 +16,10 @@
     components: {
       appStock: Stock,
     },
-    data() {
-      return {
-        stocks: [
-          { id: 1, name: 'BMW', price: 110 },
-          { id: 2, name: 'Google', price: 250 },
-          { id: 3, name: 'Apple', price: 200 },
-          { id: 4, name: 'Twitter', price: 8 },
-        ],
-      };
+    computed: {
+      stocks() {
+        return this.$store.getters.stocks;
+      }
     },
   };
 </script>
