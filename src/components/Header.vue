c3a8402f317dbbd0a687fc537249c0b9714e4bbc
Lucas Katayama <lucaskatayama@gmail.com>
2017-05-28 23:20:01 -0300

    Saving data

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -25,7 +25,7 @@
               aria-expanded="false">Save & Load <span class="caret"></span>
             </a>
             <ul class="dropdown-menu">
-              <li><a href="#">Save Data</a></li>
+              <li><a href="#" @click="saveData">Save Data</a></li>
               <li><a href="#">Load Data</a></li>
             </ul>
           </li>
@@ -55,6 +55,15 @@
       endDay() {
         this.randomizeStocks();
       },
+      saveData() {
+        const data = {
+          funds:this.$store.getters.funds,
+          stockPortfolio: this.$store.getters.stockPortfolio,
+          stocks: this.$store.getters.stocks,
+        };
+        console.log(data);
+        this.$http.put('data.json', data);
+      }
     },
   };
 </script>
