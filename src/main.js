458163f40609f2cc1a5408f6bfafd7436d93c84a
Lucas Katayama <lucaskatayama@gmail.com>
2017-05-28 23:11:59 -0300

    Adding vue-resource and Firebase

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 
 import App from './App.vue'
 import { routes } from './router';
 import store from './store/store';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-3a7c0.firebaseio.com/';
 
 Vue.filter('currency', (input) => {
   return `$ ${input.toLocaleString()}`;
